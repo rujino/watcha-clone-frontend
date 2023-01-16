e0c6603c3b10db1ba087e0fa87a5fd829971dc7a
ryuji <ryujinho63@gmail.com>
2023-01-16 19:23:41 +0900

    seriesdetail director add

--- src/routes/SeriesDetail.tsx
+++ src/routes/SeriesDetail.tsx
@@ -110,6 +110,12 @@ export default function SeriesDetail() {
               <TabPanel textAlign="left">
                 <Box>
                   <Heading mb="10px">감독/출연</Heading>
+                  {data?.director.map((directors, index) => (
+                    <HStack gap="3">
+                      <Avatar m="5px" />
+                      <Box key={index}>{directors.director_name}</Box>
+                    </HStack>
+                  ))}
                   {data?.actor.map((actors, index) => (
                     <HStack gap="3">
                       <Avatar m="5px" />
